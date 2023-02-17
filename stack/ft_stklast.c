52a9e857dee8a85834e2af431a9d2a63dba3f9fd
fgomez-d <fgomez-d@student.42malaga.com>
2023-02-17 13:08:15 +0100

    Finish stklast

--- stack/ft_stklast.c
+++ stack/ft_stklast.c
@@ -1,22 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*   ft_stklast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 11:08:02 by fgomez-d          #+#    #+#             */
-/*   Updated: 2022/12/06 16:17:49 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2023/02/17 13:06:43 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstlast(t_list *lst)
+t_stk	*ft_stklast(t_stk *stk)
 {
-	if (lst == NULL)
+	if (stk == NULL)
 		return (NULL);
-	while (lst->next != NULL)
-		lst = lst->next;
-	return (lst);
+	if (stk == stk->first)
+	{
+		if (stk->next == NULL)
+			return (stk);
+		else
+			stk = stk->next;
+	}
+	while (stk->next != stk->first)
+		stk = stk->next;
+	return (stk);
 }

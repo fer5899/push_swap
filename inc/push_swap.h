fb075f85daa7f08452c60e6d1075b336bcbbfad8
fgomez-d <fgomez-d@student.42malaga.com>
2023-03-19 14:29:23 +0100

    Rename stk elem struct and add new functions

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 12:07:46 by fgomez-d          #+#    #+#             */
-/*   Updated: 2023/03/17 13:16:55 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2023/03/19 14:06:03 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,19 @@ typedef struct s_array
 	int	size;
 }	t_array;
 
-typedef struct s_ps_stk
+typedef struct s_elem_stk
 {
+	int	pos;
 	int	value;
 	int	index;
 	int	target_pos;
 	int	cost_a;
 	int	cost_b;
-}	t_ps_stk;
+}	t_elem_stk;
 
-int	is_int(char *str);
-int	merge_sort(int arr[], int size);
+int			check_args(int argc, char **argv);
+int			merge_sort(int arr[], int size);
+t_stk		*create_stk(int argc, char **argv);
+t_elem_stk	*new_elem_stk(int value, int index);
 
 #endif

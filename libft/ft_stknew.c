d9781274d9859481e3888524185de05bb95eb734
fgomez-d <fgomez-d@student.42malaga.com>
2023-03-19 14:26:24 +0100

    Null protection for stknew

--- libft/ft_stknew.c
+++ libft/ft_stknew.c
@@ -6,7 +6,7 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 10:30:33 by fgomez-d          #+#    #+#             */
-/*   Updated: 2023/03/08 15:21:41 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2023/03/19 13:59:58 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_stk	*ft_stknew(void *content)
 {
 	t_stk	*node;
 
+	if (content == NULL)
+		return (NULL);
 	node = (t_stk *) malloc(sizeof(t_stk));
 	if (node == NULL)
 		return (NULL);

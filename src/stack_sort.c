7fe347d3f820a36a5a8be98e217257b6268dbc27
fgomez-d <fgomez-d@student.42malaga.com>
2023-03-21 19:51:24 +0100

    Create functions for two and three element sort

---
+++ src/stack_sort.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 17:40:14 by fgomez-d          #+#    #+#             */
+/*   Updated: 2023/03/21 19:51:06 by fgomez-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void	two_elem_sort(t_stk *stk)
+{
+	t_elem_stk	*elem;
+
+	elem = (t_elem_stk *) stk->first->content;
+	if (elem->index == 2)
+		swap(stk, 'a');
+}
+
+void	three_elem_sort(t_stk *stk)
+{
+	if (((t_elem_stk *) stk->first->content)->index == 3)
+		rotate(stk, stk, 'a');
+	else if (((t_elem_stk *) stk->first->next->content)->index == 3)
+		rev_rotate(stk, stk, 'a');
+	if (((t_elem_stk *) stk->first->content)->index
+		> ((t_elem_stk *) stk->first->next->content)->index)
+		swap(stk, 'a');
+}

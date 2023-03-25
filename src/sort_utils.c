4cc362d616690597366a8f385a07e59a4dd5fa33
fgomez-d <fgomez-d@student.42malaga.com>
2023-03-25 10:28:35 +0100

    Rename to is_sorted

--- src/sort_utils.c
+++ src/sort_utils.c
@@ -6,13 +6,13 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 16:31:20 by fgomez-d          #+#    #+#             */
-/*   Updated: 2023/03/21 19:44:05 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2023/03/23 12:54:26 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-int	is_ordered(t_stk *stk)
+int	is_sorted(t_stk *stk)
 {
 	stk = stk->first;
 	while (stk->next != stk->first)
